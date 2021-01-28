814f6a1497000d6e242d29bb22f12b9a75720c64
AlexBuratta13 <alex,buratta13@gmail.com>
2021-01-28 11:14:35 +0100

    Add project files.

---
+++ Buratta_RicercaConcorrente/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buratta_RicercaConcorrente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+        public static void RicercaNumeri(int n, int[] array)
+        {
+            for (int i = 0; array.Length < 100; i++)
+            {
+                if (array[i] == n)
+                {
+                    Console.WriteLine("il numero è contenuto nell array");
+                }
+                else
+                {
+                    Console.WriteLine("il numero non esiste nell' array");
+                }
+            }
+        }
+    }
+}
