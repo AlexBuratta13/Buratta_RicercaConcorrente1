4c86dabe73252d8b0a041faa2a786e646d339504
AlexBuratta13 <alex.buratta13@gmail.com>
2021-01-29 08:40:08 +0100

    aggiunta metodo riempi array

--- Buratta_RicercaConcorrente/Program.cs
+++ Buratta_RicercaConcorrente/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Buratta_RicercaConcorrente
 {
@@ -10,20 +10,31 @@ namespace Buratta_RicercaConcorrente
     {
         static void Main(string[] args)
         {
+            int[] n = new int[100];
+            RiempiArray(n);
+            Console.WriteLine("Inserisci un numero:");
+            int n1 = int.Parse(Console.ReadLine());
+            Thread thread = new Thread(() => RicercaNumeri(n1, n));
+            thread.Start();
             Console.ReadLine();
         }
         public static void RicercaNumeri(int n, int[] array)
         {
-            for (int i = 0; array.Length < 100; i++)
+            for (int i = 0; i < 100; i++)
             {
-                if (array[i] == n)
+                if (n == array[i])
                 {
-                    Console.WriteLine("il numero è contenuto nell array");
+                    Console.WriteLine($"numero Trovato: {array[i]} posizione: {i}");
                 }
-                else
-                {
-                    Console.WriteLine("il numero non esiste nell' array");
-                }
+            }
+            Console.WriteLine("Ricerca terminata");
+        }
+        private static void RiempiArray(int[] numeri)
+        {
+            Random r = new Random();
+            for (int n = 0; n < 100; n++)
+            {
+                numeri[n] = r.Next(0, 100);
             }
         }
     }
